4486ade1376b58f701f017412496ea3aea3f2b75
Craig O'Donnell <craig@replicated.com>
2022-08-26 17:40:53 -0400

    multiline output

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,8 @@ fi
 
 RELEASE_NOTES=$(eval $CMD)
 
-echo "::set-output name=release-notes::'$RELEASE_NOTES'"
+RELEASE_NOTES="${RELEASE_NOTES//'%'/'%25'}"
+RELEASE_NOTES="${RELEASE_NOTES//$'\n'/'%0A'}"
+RELEASE_NOTES="${RELEASE_NOTES//$'\r'/'%0D'}"
+
+echo "::set-output name=release-notes::$RELEASE_NOTES"
