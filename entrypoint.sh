986f5e2c8905ea52521a051489ed7fcd1b2ebd99
Craig O'Donnell <craig@replicated.com>
2022-08-26 17:42:56 -0400

    multiline output as environment variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,8 @@ fi
 
 RELEASE_NOTES=$(eval $CMD)
 
-RELEASE_NOTES="${RELEASE_NOTES//'%'/'%25'}"
-RELEASE_NOTES="${RELEASE_NOTES//$'\n'/'%0A'}"
-RELEASE_NOTES="${RELEASE_NOTES//$'\r'/'%0D'}"
+echo "RELEASE_NOTES<<EOF" >> $GITHUB_ENV
+echo "$RELEASE_NOTES" >> $GITHUB_ENV
+echo "EOF" >> $GITHUB_ENV
 
 echo "::set-output name=release-notes::$RELEASE_NOTES"
